9c607faf3f68605a3ad3b6c4364278af52a52a6e
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2025-07-05 15:03:58 +0500

    A

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         String fileName = "output.txt";
-        String content = "Хуйло\n";
+        String content = "Добро\n";
 
         try {
             // Получаем путь к корню проекта
